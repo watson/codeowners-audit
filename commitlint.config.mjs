458ce453b2b0c334a2d390654ae0d357d9a1bbed
Thomas Watson <w@tson.dk>
2026-03-06 21:39:05 +0100

    test: use default commitlint line lengths (100)

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,8 +1,3 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'body-max-line-length': [0],
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-  },
 }
